e36dce8c8493ea932cd9e051b6aed38dc52e8c96
郭帅 <yooless@163.com>
2017-12-15 13:34:46 +0800

    commit

--- zy_crm/src/main/webapp/js/admin.js
+++ zy_crm/src/main/webapp/js/admin.js
@@ -1,9 +1,10 @@
-// 注意本项目使用了JS和jQuery，但给学生说明JS和jQuery等以后会讲，现在大家先了解即可
+﻿// 注意本项目使用了JS和jQuery，但给学生说明JS和jQuery等以后会讲，现在大家先了解即可
 
 $('.menu-title').click(function(){
 	$('.menu-title').removeClass('active');
 	$(this).addClass('active');
 });
+
 
 $('.sub-menu li').click(function(){
 	$('.sub-menu li').removeClass('active');
